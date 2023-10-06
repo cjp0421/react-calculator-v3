c04f437c1ff8fe220f47f58b2a661ef460cdafb1
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-10-05 19:51:13 -0500

    playing with elements in calculator
    added another useState hook to display original input value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+  const [input, setInput] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+
+  function minus(e) {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e) {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e) {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+
+  };
+
   return (
     <div className="App">
-      <h1>Hello Calculator!</h1>
-      <h2>something else</h2>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}> {result} </p>
+        <p ref={inputRef}>{input} </p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={(input) => {
+          input.preventDefault();
+          setInput(input = Number(inputRef.current.value))
+        }}>Set starting input</button><br />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetResult}>Reset Result</button>
+        <button onClick={resetInput}>Reset Input</button>
+      </form>
     </div>
+
   );
 }
 
